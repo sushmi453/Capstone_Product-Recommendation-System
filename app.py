f2f9c3520198a1fcdf5e327fa4af3af20dcc52ba
sushmi453 <sushmi453@gmail.com>
2022-03-31 16:49:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #from cgitb import text
 from flask import Flask,jsonify,render_template,request
 import os
-dir = os.chdir(r"C:\Users\geddamsg\Capstone_Product-Recommendation-System\nltk_data")
+dir = os.chdir(r"C:\\Users\geddamsg\\Capstone_Product-Recommendation-System\\nltk_data")
 #dir = os.chdir(nltk_data) + "/" + nltk_data
 import nltk
 nltk.data.path.append(dir)
