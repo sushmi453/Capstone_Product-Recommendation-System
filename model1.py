6aa2fb74cb685a971ee8a4eae2987442c8a53532
sushmi453 <sushmi453@gmail.com>
2022-03-31 15:07:15 +0530

    Update and rename app.py to model1.py

--- model1.py
+++ model1.py
@@ -1,14 +1,9 @@
 #from cgitb import text
 from flask import Flask,jsonify,render_template,request
-#import model 
+import model 
 import pickle as pk
 app = Flask('__name__')
 
-# load the pickle files 
-count_vector = pk.load(open('Pickle/count_vector.pkl','rb'))            # Count Vectorizer
-tfidf_transformer = pk.load(open('Pickle/tfidf_transformer.pkl','rb')) # TFIDF Transformer
-model = pk.load(open('Pickle/model.pkl','rb'))                          # Classification Model
-recommend_matrix = pk.load(open('Pickle/user_final_rating.pkl','rb'))   # User-User Recommendation System 
 
 #import model 
 
